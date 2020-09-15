211e78adf2273eff23db840ed96524b22992d490
Kunal <kunalsingh.ks384@gmail.com>
2020-09-15 18:19:39 +0530

    Add files via upload

---
+++ area of a circle.py
@@ -0,0 +1,10 @@
+print('This program is to find the area of the circle')
+a=str(input('Press 1 to continue-'))
+if a == '1' :
+   print('To find the area of the circle:-')
+   x=float(input('Enter the radius of the circle'))
+   A=3.14*x**2
+   print('The Area of the circle is ',A) 
+else:
+   print('No problem, Hope to see you again')
+   
