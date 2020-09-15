8384ad09db2cd38726e19ebbae3e96b1e58dd7ad
Kunal <kunalsingh.ks384@gmail.com>
2020-09-15 18:42:41 +0530

    Uploaded

---
+++ election eligibility.py
@@ -0,0 +1,8 @@
+print("TO check if you are eligible for voting in elections or not:-")
+A=int(input("Enter Your Age:-"))
+if (A>=18):
+    print("""You are eligible to vote in elections for your favourite candidate.\nChoose wisely the future of the country depends on your and other people's vote""")
+
+else:
+    print("""Sorry, You have to wait before you grow up and become eligible to vote""")
+
