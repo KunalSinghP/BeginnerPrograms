653bd83ce827b8d68f1416ac7096618b13ee09bd
Kunal <kunalsingh.ks384@gmail.com>
2020-09-15 18:45:06 +0530

    Uploaded
    Enter a character to check if it is a consonant or a character

---
+++ vowel or consonant.py
@@ -0,0 +1,11 @@
+ch=input("Enter a character to check whether it is a consonant or a vowel    :")
+if (ch == 'a' or 'A'):
+    print(ch," is a vowel.")
+elif (ch == 'i' or 'I'):
+    print(ch," is a vowel.")
+elif (ch == 'o' or 'O'):
+    print(ch," is a vowel.")
+elif (ch == 'u' or 'U'):
+    print(ch," is a vowel.")
+else:
+    print("consonant")
