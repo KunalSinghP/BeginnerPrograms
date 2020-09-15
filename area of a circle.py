958d52d5f52963084e4da5d3b5ba28df17f62c09
Kunal <kunalsingh.ks384@gmail.com>
2020-09-15 18:21:58 +0530

    Update area of a circle.py

--- area of a circle.py
+++ area of a circle.py
@@ -3,8 +3,8 @@ a=str(input('Press 1 to continue-'))
 if a == '1' :
    print('To find the area of the circle:-')
    x=float(input('Enter the radius of the circle'))
-   A=3.14*x**2
-   print('The Area of the circle is ',A) 
+   Area=3.14*x**2
+   print('The Area of the circle is ',Area) 
 else:
    print('No problem, Hope to see you again')
    
