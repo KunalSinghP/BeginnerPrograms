e5a9f679c88d00d8348d9e2b92ceaa82ab580f5d
Kunal <kunalsingh.ks384@gmail.com>
2020-09-16 13:00:57 +0530

    Added Comments And Time Module

--- login using dict.py
+++ login using dict.py
@@ -1,11 +1,15 @@
-d={'kunal':'ks','yash':'yc'}
-for i in range(3):
+import time #To make it look professional as it takes time to validate id and password
+
+d={'kunal':'ks','yash':'yc'} #Some already made records just to check the login system
+
+for i in range(3): #To give user 3 attempts to login in case he enters wrong credentials
     x=input('Enter UserID:')
     y=input('Password:')
+    time.sleep(2)
     if x in d:
         if d[x]==y:
             print('You are logged in.' )
-            break
+            break #If the credentials is correct, break will end the loop, otherwise the loop will run for 3 times even if u login on your first attempt.
         else:
             print('Wrong Password')
     else:
