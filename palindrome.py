b4038bcacca3e54d079f71dc573e7aa2af1c4111
Kunal <kunalsingh.ks384@gmail.com>
2020-09-16 12:55:56 +0530

    Interface Update
    Just a minor change in the last print statement

--- palindrome.py
+++ palindrome.py
@@ -3,4 +3,4 @@ if(string == string[::-1]):
     print("The string is a palindrome")
 else:
     print("The string is not a palindrome")
-print(string[::-1])
+print("The reverse of ",string," is ",string[::-1])
